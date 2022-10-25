c261b444ece8abed2fdce4b41fcb8e8f6eb2c3b7
Edroque <ebmroque77@gmail.com>
2022-10-24 21:15:02 -0300

    criado arquivos js e json

---
+++ auladegit/listadeusuarios/app.js
@@ -0,0 +1,34 @@
+/*Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros - Eduardo 
+
+Aluno 2 - Responsável por criar a função de listar todos os usuários acima de 18 anos. Erick 
+
+
+
+
+
+
+
+Aluno 3 - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true Thais kemyle
+
+
+
+
+
+
+
+
+
+
+Aluno 4 - Responsável por criar a função de adicionar um usuário. Izaias pereira
+
+
+
+
+
+
+
+
+
+
+Aluno 5- Responsável por criar a função de remover um usuário.   Eduardo */
+
