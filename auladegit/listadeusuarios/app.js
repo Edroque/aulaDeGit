332677373fd1903b188298349e81febe142eefe2
Edroque <ebmroque77@gmail.com>
2022-10-26 19:57:46 -0300

    Adicionando a função de listar usuarios acima de 18 anos

--- auladegit/listadeusuarios/app.js
+++ auladegit/listadeusuarios/app.js
@@ -1,34 +1,28 @@
 /*Aluno 1- Responsável por criar o projeto no git e dar acesso aos outros - Eduardo 
 
 Aluno 2 - Responsável por criar a função de listar todos os usuários acima de 18 anos. Erick 
-
-
-
-
-
+//filter
 
 
 Aluno 3 - Responsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true Thais kemyle
+//map e spread...
 
 
-
-
-
-
-
-
-
-
-Aluno 4 - Responsável por criar a função de adicionar um usuário. Izaias pereira
-
-
-
-
-
-
-
-
-
+Aluno 4 - Responsável por criar a função de adicionar um usuário. Hellen 
+//push
 
 Aluno 5- Responsável por criar a função de remover um usuário.   Eduardo */
+//pop
+//Tem que recever um usuario de parametro
+
+//Resposta exercio 2:
+let bancoDeDados = require('./usuarios.json')
+
+const listarUsuariosMarioresDeidade = () => {
+const lista = bancoDeDados.filter((usuario) => usuario.idade > 18)
+return lista;
+
+}
+
+console.log("lista de usuarios acima de 18: ", listarUsuariosMarioresDeidade());
 
